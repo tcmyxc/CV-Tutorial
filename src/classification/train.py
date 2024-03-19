e669e05ef6ca1672fd2b0b17c90a922d531f4428
tcmyxc <1282494272@qq.com>
2024-03-19 10:21:09 +0800

    Update train.py

--- src/classification/train.py
+++ src/classification/train.py
@@ -280,7 +280,9 @@ def main(args):
     elif args.lr_scheduler == "multisteplr":
         if "cifar" in args.data_name:
             if args.epochs == 200:
-                main_lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60, 120, 160], gamma=0.2)
+                cifar_milestones = [60, 120, 160]
+                cifar_milestones = [(i - args.lr_warmup_epochs) for i in cifar_milestones]
+                main_lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=cifar_milestones, gamma=0.2)
             elif args.epochs == 300:
                 main_lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150, 225], gamma=0.1)
         elif args.data_name == "svhn":
