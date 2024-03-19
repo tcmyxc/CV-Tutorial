6cde5b368df0d4c1682c00868fab257dfbbb189c
tcmyxc <1282494272@qq.com>
2024-03-19 22:59:28 +0800

    Update test_model_register.py

--- src/mytest/test_model_register.py
+++ src/mytest/test_model_register.py
@@ -1,14 +1,14 @@
 import argparse
 import torch
 from torchsummary import summary
-
+from ptflops import get_model_complexity_info
 from models import list_models, load_model
 
 print(list_models())
 
 parser = argparse.ArgumentParser()
 # 模型架构
-parser.add_argument("--model", default="resnet50_A1", type=str, help="model name")
+parser.add_argument("--model", default="resnet50_E", type=str, help="model name")
 parser.add_argument(
     "--model_lib",
     default="custom", type=str,
@@ -25,6 +25,17 @@ args = parser.parse_args()
 
 model = load_model(args, num_classes=100)
 print(model)
-summary(model, input_size=(3, 32, 32), batch_size=8, device="cpu")
+input_size = (3, 32, 32)
+summary(model, input_size=input_size, batch_size=8, device="cpu")
 
-print(model(torch.rand(8, 3, 32, 32)).shape)
+# with torch.cuda.device(0):
+macs, params = get_model_complexity_info(
+    model,
+    input_size,
+    as_strings=True,
+    print_per_layer_stat=False,
+    verbose=False,
+    # flops_units="GMac",
+)
+print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+print('{:<30}  {:<8}'.format('Number of parameters: ', params))
