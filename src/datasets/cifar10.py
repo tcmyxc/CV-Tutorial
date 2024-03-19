42ba2a302cb30e906af5b5b7659af5b4958673ac
tcmyxc <1282494272@qq.com>
2024-03-19 15:13:27 +0800

    Update cifar10.py

--- src/datasets/cifar10.py
+++ src/datasets/cifar10.py
@@ -13,23 +13,34 @@ from .cutout import Cutout
 
 _MEAN, _STD = (0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)
 
-train_transform = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    CIFAR10Policy(),
-    transforms.ToTensor(),
-    Cutout(n_holes=1, length=16),
-    transforms.Normalize(_MEAN, _STD),
-])
-
 val_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(_MEAN, _STD),
 ])
 
 
-def get_cifar10(data_root="~/datasets", **kwargs):
+def get_cifar10(data_root="~/datasets", random_erase_prob=0.0, auto_augment=False, cutout=False, **kwargs):
     num_classes = 10
+
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+    ])
+    if auto_augment:
+        train_transform.transforms.append(CIFAR10Policy)
+
+    train_transform.transforms.append(transforms.ToTensor())
+
+    if cutout:
+        train_transform.transforms.append(Cutout(n_holes=1, length=16))
+
+    train_transform.transforms.append(transforms.Normalize(_MEAN, _STD))
+
+    if random_erase_prob > 0 and cutout is False:
+        train_transform.transforms.append(transforms.RandomErasing(p=random_erase_prob,
+                                                                   scale=(0.125, 0.2),
+                                                                   ratio=(0.99, 1.0)))
+
     train_dataset = datasets.CIFAR10(
         root=data_root,
         train=True,
