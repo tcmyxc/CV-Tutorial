a7694c2c40543eff5da7877473d40cd2024d0b7b
tcmyxc <1282494272@qq.com>
2024-04-19 18:11:44 +0800

    Update se_module.py

--- src/models/se_module.py
+++ src/models/se_module.py
@@ -6,7 +6,7 @@ from torch import nn
 
 
 class SELayer(nn.Module):
-    def __init__(self, channel, reduction=16):
+    def __init__(self, channel, reduction=4):
         super(SELayer, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
