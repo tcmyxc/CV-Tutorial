abfb435d8783c8c24686da489a2664fde8d6d8ea
tcmyxc <1282494272@qq.com>
2024-04-19 19:00:56 +0800

    Update googlenet.py

--- src/models/cifar100/googlenet.py
+++ src/models/cifar100/googlenet.py
@@ -11,28 +11,31 @@
 
 import torch
 import torch.nn as nn
+from functools import partial
 
 from models._api import register_model
 
 class Inception(nn.Module):
-    def __init__(self, input_channels, n1x1, n3x3_reduce, n3x3, n5x5_reduce, n5x5, pool_proj):
+    def __init__(self, input_channels, n1x1, n3x3_reduce, n3x3, n5x5_reduce, n5x5, pool_proj, act_layer=None,):
         super().__init__()
+
+        act_layer = act_layer or partial(nn.ReLU, inplace=True)
 
         # 1x1conv branch
         self.b1 = nn.Sequential(
             nn.Conv2d(input_channels, n1x1, kernel_size=1),
             nn.BatchNorm2d(n1x1),
-            nn.ReLU(inplace=True)
+            act_layer()
         )
 
         # 1x1conv -> 3x3conv branch
         self.b2 = nn.Sequential(
             nn.Conv2d(input_channels, n3x3_reduce, kernel_size=1),
             nn.BatchNorm2d(n3x3_reduce),
-            nn.ReLU(inplace=True),
+            act_layer(),
             nn.Conv2d(n3x3_reduce, n3x3, kernel_size=3, padding=1),
             nn.BatchNorm2d(n3x3),
-            nn.ReLU(inplace=True)
+            act_layer()
         )
 
         # 1x1conv -> 5x5conv branch
@@ -42,13 +45,13 @@ class Inception(nn.Module):
         self.b3 = nn.Sequential(
             nn.Conv2d(input_channels, n5x5_reduce, kernel_size=1),
             nn.BatchNorm2d(n5x5_reduce),
-            nn.ReLU(inplace=True),
+            act_layer(),
             nn.Conv2d(n5x5_reduce, n5x5, kernel_size=3, padding=1),
             nn.BatchNorm2d(n5x5, n5x5),
-            nn.ReLU(inplace=True),
+            act_layer(),
             nn.Conv2d(n5x5, n5x5, kernel_size=3, padding=1),
             nn.BatchNorm2d(n5x5),
-            nn.ReLU(inplace=True)
+            act_layer()
         )
 
         # 3x3pooling -> 1x1conv
@@ -57,7 +60,7 @@ class Inception(nn.Module):
             nn.MaxPool2d(3, stride=1, padding=1),
             nn.Conv2d(input_channels, pool_proj, kernel_size=1),
             nn.BatchNorm2d(pool_proj),
-            nn.ReLU(inplace=True)
+            act_layer()
         )
 
     def forward(self, x):
@@ -66,24 +69,25 @@ class Inception(nn.Module):
 
 class GoogleNet(nn.Module):
 
-    def __init__(self, num_classes=100, **kwargs):
+    def __init__(self, num_classes=100, act_layer=None, **kwargs):
         super().__init__()
+        act_layer = act_layer or partial(nn.ReLU, inplace=True)
         self.prelayer = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
+            act_layer(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
+            act_layer(),
             nn.Conv2d(64, 192, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(192),
-            nn.ReLU(inplace=True),
+            act_layer(),
         )
 
         # although we only use 1 conv layer as prelayer,
         # we still use name a3, b3.......
-        self.a3 = Inception(192, 64, 96, 128, 16, 32, 32)
-        self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
+        self.a3 = Inception(192, 64, 96, 128, 16, 32, 32, act_layer)
+        self.b3 = Inception(256, 128, 128, 192, 32, 96, 64, act_layer)
 
         ##"""In general, an Inception network is a network consisting of
         ##modules of the above type stacked upon each other, with occasional
@@ -91,14 +95,14 @@ class GoogleNet(nn.Module):
         ##grid"""
         self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
 
-        self.a4 = Inception(480, 192, 96, 208, 16, 48, 64)
-        self.b4 = Inception(512, 160, 112, 224, 24, 64, 64)
-        self.c4 = Inception(512, 128, 128, 256, 24, 64, 64)
-        self.d4 = Inception(512, 112, 144, 288, 32, 64, 64)
-        self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
+        self.a4 = Inception(480, 192, 96, 208, 16, 48, 64, act_layer)
+        self.b4 = Inception(512, 160, 112, 224, 24, 64, 64, act_layer)
+        self.c4 = Inception(512, 128, 128, 256, 24, 64, 64, act_layer)
+        self.d4 = Inception(512, 112, 144, 288, 32, 64, 64, act_layer)
+        self.e4 = Inception(528, 256, 160, 320, 32, 128, 128, act_layer)
 
-        self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
-        self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
+        self.a5 = Inception(832, 256, 160, 320, 32, 128, 128, act_layer)
+        self.b5 = Inception(832, 384, 192, 384, 48, 128, 128, act_layer)
 
         # input feature size: 8*8*1024
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
@@ -136,6 +140,6 @@ class GoogleNet(nn.Module):
         return x
 
 
-@register_model()
+@register_model("googlenet_c100")
 def googlenet(**kwargs):
     return GoogleNet(**kwargs)
