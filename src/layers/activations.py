d7a899ef5badb6e15eb3874a8d2dded366555ca7
tcmyxc <1282494272@qq.com>
2024-04-21 01:34:51 +0800

    Update activations.py

--- src/layers/activations.py
+++ src/layers/activations.py
@@ -59,4 +59,31 @@ class SequecialHGELUV2(nn.Module):
         # 残差学习
         weight = torch.where(p_out < 0.5, p_out, 1 - p_out)
 
-        return weight * x
+        return weight * x * 2
+
+
+class SequecialHGELUV3(nn.Module):
+    """
+    串行实现版本，不包含 ReLU层
+    """
+    def __init__(
+            self,
+            num_features: int,
+            eps: float = 1e-5,
+    ) -> None:
+        super().__init__()
+        self.mu = nn.Parameter(torch.zeros(1, num_features))
+        self.log_var = nn.Parameter(torch.zeros(1, num_features))
+        self.eps = eps
+
+    def forward(self, x):
+        # 计算标准差
+        std = torch.exp(0.5 * self.log_var)
+        # 归一化
+        x = (x - self.mu.reshape(1, -1, 1, 1)) / (std.reshape(1, -1, 1, 1) + self.eps)
+        # 计算概率
+        p_out = 0.5 * (1 + torch.erf(x / math.sqrt(2)))
+        # 残差学习
+        weight = torch.where(p_out < 0.5, p_out, 1 - p_out)
+
+        return weight * x * 2
