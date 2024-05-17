5a534c39b3f8b71cc0201b66125921cd1a3d9ff6
tcmyxc <1282494272@qq.com>
2024-05-17 10:11:59 +0800

    Update wideresidual.py

--- src/models/cifar100/wideresidual.py
+++ src/models/cifar100/wideresidual.py
@@ -6,7 +6,7 @@ from models._api import register_model
 
 class WideBasic(nn.Module):
 
-    def __init__(self, in_channels, out_channels, stride=1, act_layer=None,):
+    def __init__(self, in_channels, out_channels, stride=1, act_layer=None, drop_rate=0.5,):
         super().__init__()
         self.residual = nn.Sequential(
             nn.BatchNorm2d(in_channels),
@@ -20,7 +20,7 @@ class WideBasic(nn.Module):
             ),
             nn.BatchNorm2d(out_channels),
             act_layer(),
-            nn.Dropout(),
+            nn.Dropout(p=drop_rate),
             nn.Conv2d(
                 out_channels,
                 out_channels,
@@ -45,7 +45,7 @@ class WideBasic(nn.Module):
 
 
 class WideResNet(nn.Module):
-    def __init__(self, block=WideBasic, depth=50, widen_factor=1, num_classes=100, act_layer=None, **kwargs):
+    def __init__(self, block=WideBasic, depth=50, widen_factor=1, num_classes=100, act_layer=None, drop_rate=0.5, **kwargs):
         super().__init__()
         act_layer = act_layer or partial(nn.ReLU, inplace=True)
 
@@ -54,9 +54,9 @@ class WideResNet(nn.Module):
         l = int((depth - 4) / 6)
         self.in_channels = 16
         self.init_conv = nn.Conv2d(3, self.in_channels, 3, 1, padding=1)
-        self.conv2 = self._make_layer(block, 16 * k, l, 1, act_layer)
-        self.conv3 = self._make_layer(block, 32 * k, l, 2, act_layer)
-        self.conv4 = self._make_layer(block, 64 * k, l, 2, act_layer)
+        self.conv2 = self._make_layer(block, 16 * k, l, 1, act_layer, drop_rate)
+        self.conv3 = self._make_layer(block, 32 * k, l, 2, act_layer, drop_rate)
+        self.conv4 = self._make_layer(block, 64 * k, l, 2, act_layer, drop_rate)
         self.bn = nn.BatchNorm2d(64 * k)
         self.act = act_layer()
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
@@ -75,7 +75,7 @@ class WideResNet(nn.Module):
 
         return x
 
-    def _make_layer(self, block, out_channels, num_blocks, stride, act_layer):
+    def _make_layer(self, block, out_channels, num_blocks, stride, act_layer, drop_rate):
         """make resnet layers(by layer i didnt mean this 'layer' was the
         same as a neuron netowork layer, ex. conv layer), one layer may
         contain more than one residual block
@@ -95,7 +95,7 @@ class WideResNet(nn.Module):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride, act_layer))
+            layers.append(block(self.in_channels, out_channels, stride, act_layer, drop_rate))
             self.in_channels = out_channels
 
         return nn.Sequential(*layers)
@@ -106,7 +106,7 @@ def wide_resnet40_10(**kwargs):
     """
     for cifar10/cifar100
     """
-    net = WideResNet(depth=40, widen_factor=10, dropRate=0.3, **kwargs)
+    net = WideResNet(depth=40, widen_factor=10, drop_rate=0.3, **kwargs)
     return net
 
 
@@ -115,7 +115,7 @@ def wide_resnet28_10(**kwargs):
     """
     for cifar10/cifar100
     """
-    net = WideResNet(depth=28, widen_factor=10, dropRate=0.3, **kwargs)
+    net = WideResNet(depth=28, widen_factor=10, drop_rate=0.3, **kwargs)
     return net
 
 @register_model("WRN_16_8_c100")
@@ -123,6 +123,6 @@ def wide_resnet16_8(**kwargs):
     """
     for svhn
     """
-    net = WideResNet(depth=16, widen_factor=8,dropRate=0.4, **kwargs)
+    net = WideResNet(depth=16, widen_factor=8, drop_rate=0.4, **kwargs)
     return net
 
