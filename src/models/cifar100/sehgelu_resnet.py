3548c29197461aba785705643ba5ec3e0e357839
tcmyxc <1282494272@qq.com>
2024-05-24 15:54:02 +0800

    Update sehgelu_resnet.py

--- src/models/cifar100/sehgelu_resnet.py
+++ src/models/cifar100/sehgelu_resnet.py
@@ -15,6 +15,49 @@ from models._api import register_model
 from layers import SequecialHGELUV4
 
 
+class BasicBlock(nn.Module):
+    """Basic Block for resnet 18 and resnet 34
+
+    """
+
+    #BasicBlock and BottleNeck block
+    #have different output size
+    #we use class attribute expansion
+    #to distinct
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1, act_layer=None,):
+        super().__init__()
+
+        self.act_layer = act_layer or partial(nn.ReLU, inplace=True)
+
+        #residual function
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            act_layer(),
+            nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels * BasicBlock.expansion)
+        )
+        self.aa = SequecialHGELUV4(out_channels * BasicBlock.expansion)
+
+        #shortcut
+        self.shortcut = nn.Sequential()
+
+        #the shortcut output dimension is not the same with residual function
+        #use 1*1 convolution to match the dimension
+        if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * BasicBlock.expansion)
+            )
+
+    def forward(self, x):
+        out = self.residual_function(x)
+        out = out + self.aa(out)
+        return self.act_layer()(out + self.shortcut(x))
+
+
 class BottleNeck(nn.Module):
     """Residual block for resnet over 50 layers
 
