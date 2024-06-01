8f7607bd8582c933d5c1026563273ee6598dc312
tcmyxc <1282494272@qq.com>
2024-06-01 20:42:57 +0800

    modify

---
+++ src/imagenet_classification/resnet50_train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for model in 'resnet50_torch'
+do
+    CUDA_VISIBLE_DEVICES="0,1" torchrun --nproc_per_node=2  --master_port="28975" imagenet_classification/train.py \
+        --lr 0.1 \
+        --batch-size 128 \
+        --model ${model} \
+        --cache-dataset \
+        --print-freq 100 \
+        --lr-warmup-epochs 5 \
+        --amp \
+        --output-dir ./work_dir_155/imagenet/${model} \
+        --data-path /nfs/xwx/dataset/ImageNet-1k \
+        > $(date "+%Y%m%d-%H%M%S")-${model}.log
+
+    wait
+done
