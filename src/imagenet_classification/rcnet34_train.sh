5ae7b958f2254a35ed8363dadf68e7b83f59b58d
tcmyxc <1282494272@qq.com>
2024-06-10 16:15:43 +0800

    modify

---
+++ src/imagenet_classification/rcnet34_train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for model in 'rcnet34'
+do
+    CUDA_VISIBLE_DEVICES="0,1" torchrun --nproc_per_node=2  --master_port="26935" imagenet_classification/train.py \
+        --lr 0.1 \
+        --batch-size 128 \
+        --model ${model} \
+        --cache-dataset \
+        --print-freq 100 \
+        --lr-warmup-epochs 5 \
+        --amp \
+        --output-dir ./work_dir_155/imagenet/${model} \
+        --data-path /mnt/nfs/cv_datasets/ILSVRC2012 \
+        > $(date "+%Y%m%d-%H%M%S")-${model}.log
+
+    wait
+done
