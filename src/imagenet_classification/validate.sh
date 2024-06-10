f08a07ce276828bbb5a2aea40b22ed3e94f4cd3e
tcmyxc <1282494272@qq.com>
2024-06-10 16:32:10 +0800

    modify

--- src/imagenet_classification/validate.sh
+++ src/imagenet_classification/validate.sh
@@ -7,8 +7,7 @@ CUDA_VISIBLE_DEVICES="0" torchrun --nproc_per_node=1  --master_port="22975" imag
     --batch-size 256 \
     --model ${model} \
     --cache-dataset \
-    --print-freq 50 \
     --resume ${weight_path} \
     --output-dir ./work_dir_155/imagenet/${model} \
-    --data-path /disk1/wangyi/datasets/classification/ILSVRC2012
+    --data-path /nfs/xwx/dataset/ImageNet-1k
 
