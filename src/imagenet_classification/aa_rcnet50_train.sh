80ac89704c9e59dc17dfb1f80b76c74d9c173f93
tcmyxc <1282494272@qq.com>
2024-06-15 01:02:38 +0800

    modify

--- src/imagenet_classification/aa_rcnet50_train.sh
+++ src/imagenet_classification/aa_rcnet50_train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# ref: https://pytorch.org/blog/how-to-train-state-of-the-art-models-using-torchvision-latest-primitives/
+
 for model in 'rcnet50'
 do
     CUDA_VISIBLE_DEVICES="0,1" torchrun --nproc_per_node=2  --master_port="21975" imagenet_classification/train.py \
@@ -9,10 +11,21 @@ do
         --print-freq 100 \
         --lr-scheduler cosineannealinglr \
         --lr-warmup-epochs 5 \
+        --auto-augment ta_wide \
+        --epochs 600 \
+        --random-erase 0.1 \
         --label-smoothing 0.1 \
-        --auto-augment imagenet \
+        --mixup-alpha 0.2 \
+        --cutmix-alpha 1.0 \
+        --weight-decay 2e-5 \
+        --norm-weight-decay 0.0 \
+        --train-crop-size 176 \
+        --model-ema \
+        --val-resize-size 232 \
+        --ra-sampler \
+        --ra-reps 4 \
         --amp \
-        --output-dir ./work_dir_155/imagenet/cosine_ls_aa/${model} \
+        --output-dir ./work_dir_155/imagenet/aa/${model} \
         --data-path /disk1/wangyi/datasets/classification/ILSVRC2012  \
         > $(date "+%Y%m%d-%H%M%S")-${model}.log
 
